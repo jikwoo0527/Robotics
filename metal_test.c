3b0b47b6676a3c630a2bc195777ba065ea4e16f8
jikwoo0527 <162891330+jikwoo0527@users.noreply.github.com>
2024-11-27 12:55:57 +0900

    [fix] if 문 논리 수정

--- metal_test.c
+++ metal_test.c
@@ -3,17 +3,17 @@ void setup() {
 }
 
 void loop() {
-  int THRESHOLD = 10;
+  int THRESHOLD = 5;
   float measure = analogRead(0) // 아날로그 입력 신호 받음
 
   float metal_val = measure * 5.0 / 1024.0; // 아날로그 입력 신호를 실제 전압으로 변환
   Serial.println(metal_val); // 실제 전압 값 출력
 
   if(metal_val > THRESHOLD) {
-    Serial.println("Motor Activate");
+    Serial.println("Right Motor Activate");
   } 
   else {
-    Serial.println("Motor Deactivate");
+    Serial.println("Left Motor Deactivate");
   }
 
   Serial.println();
